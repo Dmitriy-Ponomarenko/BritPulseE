a07513dc3a12bf829da518abbbb58e8fdddffb1d
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-05-01 11:55:51 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Flipbook from "./Components/Flipbook";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Flipbook />
+    </div>
+  );
+}
+
+export default App;
